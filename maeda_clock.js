42d91fb432a93301b6b24a96ba910f97bd3911f2
Eviee <evesaysmeow13@gmail.com>
2024-02-29 11:14:00 +1300

    Update maeda_clock.js

--- maeda_clock.js
+++ maeda_clock.js
@@ -3,7 +3,24 @@ function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
   background(50); //  beige
   fill(200); // dark grey
-  textSize(40);
+  textSize(190);
   textAlign(CENTER, CENTER);
-  text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
+  fill(0);
+  text("10", width/2, height/2);
+
+  translate(width / 2, height / 2);
+  push()
+  angleMode(DEGREES);
+  rotate(360 / 12);
+  textSize(120);
+  fill(1000);
+  text("57", 0, 0);
+  pop()
+
+  push()
+  rotate(360 / 1.1);
+  textSize(70);
+  fill(252,3,3);
+  text("40", 0, 0);
+  pop()
 }
