57ea09906aa4d4d72b377ddee530d8b34c9a86b5
Eviee <evesaysmeow13@gmail.com>
2024-03-07 11:00:36 +1300

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,7 +1,13 @@
 /*
  * use p5.js to draw a clock on a 960x500 canvas
  */
+let Sunpoints;
+
+function preload() {
+  Sunpoints = loadImage('assets/sunpoints.png');
+}
 function draw_clock(obj) {
+
   // draw your own clock here based on the values of obj:
   //    obj.hours goes from 0-23
   //    obj.minutes goes from 0-59
@@ -11,28 +17,22 @@ function draw_clock(obj) {
   //        < 0 if no alarm is set
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
-  background(50); //  beige
-  fill(200); // dark grey
-  noStroke();
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  // text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
+  background(50);
+  angleMode (DEGREES);
+
  
-
-  Sunpoints = loadImage('assets/sunpoints.png');
-
   
   stroke('#ffffff');
   strokeWeight(2);
   noFill();
 
 
-  ellipse(480, 250, 360);
+  ellipse(480, 250, 300);
 
   fill('#ebd294');
-  ellipse(480, 250, 250);
+  ellipse(480, 250, 220);
   fill(50);
-  ellipse(480, 225, 200);
+  ellipse(480, 231, 180);
 
   noFill();
   // ellipse(480, 700, 900, 900); 
@@ -40,12 +40,6 @@ function draw_clock(obj) {
   ellipse(-50, 250, 500);
 
 
-  image(Sunpoints, 0, 0, 1000, 1000);
-  // fill(249, 140, 255);// pink
-  // ellipse(width / 3, 350, 150);
-  // fill(140, 255, 251) // blue
-  // ellipse(width / 2, 350, 150);
-  // fill(175, 133, 255); // purple
-  // ellipse(width / 3 * 2, 350, 150);
-
+  image(Sunpoints, 170, -60, 620, 620);
+  
 }
